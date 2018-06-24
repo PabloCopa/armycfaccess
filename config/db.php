800df5c9f1ac32e2a459a415af7f900332396e40
Pablo <pabloxplod@gmail.com>
2018-06-24 01:44:36 -0300

    fix
    again...

--- config/db.php
+++ config/db.php
@@ -1,10 +1,10 @@
 <?php
 
-$host = '';
-$user = '';
-$password = '';
-$dbname = '';
-$port='';
+$host = 'ec2-54-163-229-212.compute-1.amazonaws.com';
+$user = 'lwnwsibmeluecn';
+$password = 'fbd10d0e0bf159244e530cecc56b3260864a4cabbb0a09d0b9ad00dae1b2917f';
+$dbname = 'db1dtmnbmah579';
+$port='5432';
 
 try{
   //Set DSN data source name
