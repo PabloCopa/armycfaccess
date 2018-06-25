716d97d6d558bb047ab8065fc8c9c048a5e08655
Pablo <pabloxplod@gmail.com>
2018-06-25 20:14:15 -0300

    fix?

---
+++ config/db.php
@@ -0,0 +1,23 @@
+<?php
+
+$host = 'ec2-54-163-229-212.compute-1.amazonaws.com';
+$user = 'lwnwsibmeluecn';
+$password = 'fbd10d0e0bf159244e530cecc56b3260864a4cabbb0a09d0b9ad00dae1b2917f';
+$dbname = 'db1dtmnbmah579';
+$port='5432';
+
+try{
+  //Set DSN data source name
+    $dsn = "pgsql:host=ec2-54-163-229-212.compute-1.amazonaws.com" . $host . ";port=5432" . $port .";dbname=db1dtmnbmah579" . $dbname . ";user=lwnwsibmeluecn" . $user . ";password=fbd10d0e0bf159244e530cecc56b3260864a4cabbb0a09d0b9ad00dae1b2917f" . $password . ";";
+
+
+  //create a pdo instance
+  $pdo = new PDO($dsn, $user, $password);
+  $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE,PDO::FETCH_OBJ);
+  $pdo->setAttribute(PDO::ATTR_EMULATE_PREPARES,false);
+  $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}
+catch (PDOException $e) {
+echo 'Connection failed: ' . $e->getMessage();
+}
+  ?>
