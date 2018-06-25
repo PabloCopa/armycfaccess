6396a624b1e0498f1a3970939076357dcc5a4528
Pablo <pabloxplod@gmail.com>
2018-06-25 20:21:04 -0300

    fix

--- index.php
+++ index.php
@@ -32,7 +32,7 @@
 <?php
   //echo 'This is Index Page';
 
-  $sql = 'SELECT * FROM users';
+  $sql = 'SELECT * FROM personas';
   $stmt = $pdo->prepare($sql);
   $stmt->execute();
   $rowCount = $stmt->rowCount();
