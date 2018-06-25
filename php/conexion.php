3e50c6841dea56618ba0b93b28aa7da70bfcf3e0
Pablo <pabloxplod@gmail.com>
2018-06-24 21:22:35 -0300

    fix

--- php/conexion.php
+++ php/conexion.php
@@ -4,6 +4,6 @@ $user="lwnwsibmeluecn";
 $password="fbd10d0e0bf159244e530cecc56b3260864a4cabbb0a09d0b9ad00dae1b2917f";
 $db="db1dtmnbmah579";
 $port="5432";
-$con = new mysqli($host,$user,$password,$db);
+$con = new mysqli($host,$user,$password,$db,$port);
 
 ?>
