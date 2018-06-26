e5000b9dd83ed6f73030fdc1ff58d5149649d665
Pablo <pabloxplod@gmail.com>
2018-06-26 19:35:00 -0300

    fix DB

--- admin/includes/Database.php
+++ admin/includes/Database.php
@@ -15,7 +15,7 @@ class Database extends PDO {
     public function __construct() {
 
         try {
-            parent::__construct(DB_TYPE . ':host=' . DB_HOST . ';dbname=' . DB_NAME . ';charset=utf8', DB_USER, DB_PASS);
+            parent::__construct(DB_TYPE . 'pgsql:host=' . DB_HOST . ';dbname=' . DB_NAME . ';charset=utf8', DB_USER, DB_PASS);
             $this->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         } catch (PDOException $e) {
             $custom_errormsg = 'Error connecting to database - check your database connection properties in constants.php file';
