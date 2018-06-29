bddf557d43e49bd5e0663769987cf049ecb4a0c8
Pablo <pabloxplod@gmail.com>
2018-06-29 18:55:45 -0300

    404

--- php/agregar.php
+++ php/agregar.php
@@ -1,16 +1,16 @@
 <?php
 
 if(!empty($_POST)){
-	if(isset($_POST["name"]) &&isset($_POST["lastname"]) &&isset($_POST["email"]) &&isset($_POST["address"]) &&isset($_POST["phone"])){
-		if($_POST["name"]!=""&& $_POST["lastname"]!=""&&$_POST["address"]!=""){
-			include "conexion.php";
+	if(isset($_POST['name']) &&isset($_POST['lastname']) &&isset($_POST['email']) &&isset($_POST['address']) &&isset($_POST['phone'])){
+		if($_POST['name']!=''&& $_POST['lastname']!=''&&$_POST['address']!=''){
+			include 'conexion.php';
 			
-			$psql = "insert into person(name,lastname,email,address,phone,created_at) value (\"$_POST[name]\",\"$_POST[lastname]\",\"$_POST[email]\",\"$_POST[address]\",\"$_POST[phone]\",NOW())";
-			$query = $pdo->query($psql);
+			$psql = 'insert into person(name,lastname,email,address,phone,created_at) value (\'$_POST[name]\',\'$_POST[lastname]\',\'$_POST[email]\',\'$_POST[address]\',\'$_POST[phone]\',NOW())';
+			$query = $dsn->query($psql);
 			if($query!=null){
-				print "<script>alert(\"Agregado exitosamente.\");window.location='../ver.php';</script>";
+				print ''<script>alert(\'Agregado exitosamente.\');window.location='../ver.php';</script>';
 			}else{
-				print "<script>alert(\"No se pudo agregar.\");window.location='../ver.php';</script>";
+				print '<script>alert(\'No se pudo agregar.\');window.location='../ver.php';</script>';
 
 			}
 		}
