26ebc426adaebee4e121a74a454c3b8414e22f1d
Pablo <pabloxplod@gmail.com>
2018-06-30 20:19:44 -0300

    404

--- php/agregar.php
+++ php/agregar.php
@@ -1,12 +1,15 @@
 <?php
-
 if(!empty($_POST)){
 	if(isset($_POST['name']) &&isset($_POST['lastname']) &&isset($_POST['email']) &&isset($_POST['address']) &&isset($_POST['phone'])){
-		if($_POST['name']!=''&& $_POST['lastname']!=''&&$_POST['address']!=''){
+		if($_POST['name']!=''&& $_POST['lastname']!=''&&$_POST['address']!='')
+        
+        {
 			include 'conexion.php';
 			
-			$sql = "insert into person (name,lastname,email,address,phone,created_at) values (\'$_POST[name]\',\'$_POST[lastname]\',\'$_POST[email]\',\'$_POST[address]\',\'$_POST[phone]\',NOW())";
-			$query = pg_query($dbconn,'person' , $sql);
+			$sql = "insert into person (name,lastname,email,address,phone,created_at) values ('$_POST[name]','$_POST[lastname]','$_POST[email]','$_POST[address]','$_POST[phone]',NOW())";
+            
+			$query = pg_query($dbconn,$sql);
+            
 			if($query!=null){
 				print "<script>alert(\"Agregado exitosamente.\");window.location='../ver.php';</script>";
 			}else{
