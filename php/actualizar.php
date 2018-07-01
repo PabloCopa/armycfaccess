deffce70f15e0d4950f212e6d6f5dfc15499fda7
Pablo <pabloxplod@gmail.com>
2018-07-01 03:39:10 -0300

    Fix

--- php/actualizar.php
+++ php/actualizar.php
@@ -5,8 +5,10 @@ if(!empty($_POST)){
 		if($_POST["name"]!=""&& $_POST["lastname"]!=""&&$_POST["address"]!=""){
 			include "conexion.php";
 			
-			$sql = "UPDATE person SET name=\"$_POST[name]\",lastname=\"$_POST[lastname]\",email=\"$_POST[email]\",address=\"$_POST[address]\",phone=\"$_POST[phone]\" WHERE id=".$_POST["id"];
+			$sql = "UPDATE person SET name=\'$_POST[name]\',lastname=\'$_POST[lastname]\',email=\'$_POST[email]\',address=\'$_POST[address]\',phone=\'$_POST[phone]\' WHERE id=".$_POST[id];
+            
 			$query = pg_query($dbconn, $sql);
+            
 			if($query!=null){
 				print "<script>alert(\"Actualizado exitosamente.\");window.location='../ver.php';</script>";
 			}else{
