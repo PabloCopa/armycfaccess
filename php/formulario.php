b9479045f72efec8eaeeac11d8818b1e265585a9
Pablo <pabloxplod@gmail.com>
2018-07-01 18:39:48 -0300

    Fix

--- php/formulario.php
+++ php/formulario.php
@@ -5,9 +5,11 @@ $user_id=null;
 $sql1= "select * from person where id = ".$_GET["id"];
 $query = pg_query($dbconn, $sql1);
 $person = null;
-if($query->num_rows>0){
-while ($r=$query->fetch_object()){
-  $person=$r;
+?>
+
+<? if(pg_num_rows($query)>0){
+while ($row= pg_fetch_object($query)){
+  $person=$row;
   break;
 }
 
