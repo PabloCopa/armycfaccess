075c350faa361e075bcb39c85f710e3018cf5d9c
Pablo <pabloxplod@gmail.com>
2018-07-02 00:11:37 -0300

    Fix

--- ver.php
+++ ver.php
@@ -17,32 +17,32 @@
   <!-- Modal -->
   <div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
     <div class="modal-dialog">
-      <div class="modal-content">
+      <div class="modal-content" style="background-color: #8fa589de;">
         <div class="modal-header">
           <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
-          <h4 class="modal-title">Agregar</h4>
+          <h4 class="modal-title">AGREGAR USUARIO</h4>
         </div>
         <div class="modal-body">
             <form role="form" method="post" action="php/agregar.php">
               <div class="form-group">
                 <label for="name">Nombre</label>
-                <input type="text" class="form-control" name="name" required>
+                <input type="text" class="form-control" placeholder="Box&Gym" name="name" required>
               </div>
               <div class="form-group">
                 <label for="lastname">Apellido</label>
-                <input type="text" class="form-control" name="lastname" required>
+                <input type="text" class="form-control" placeholder="Army" name="lastname" required>
               </div>
               <div class="form-group">
                 <label for="address">Domicilio</label>
-                <input type="text" class="form-control" name="address" required>
+                <input type="text" class="form-control" placeholder="Castelli 266" name="address" required>
               </div>
               <div class="form-group">
                 <label for="email">Email</label>
-                <input type="email" class="form-control" name="email" >
+                <input type="email" class="form-control" placeholder="army@boxgym.com" name="email" data-error="Ingrese un Email valido." required>
               </div>
               <div class="form-group">
                 <label for="phone">Telefono</label>
-                <input type="text" class="form-control" name="phone" >
+                <input type="text" class="form-control" placeholder="12345678" name="phone" required>
               </div>
 
               <button type="submit" class="btn btn-default">Agregar</button>
