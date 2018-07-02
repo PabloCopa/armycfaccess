e2afbe3d5d02facc7acc055a15762b07f250839a
Pablo <pabloxplod@gmail.com>
2018-07-02 14:37:12 -0300

    Fix Js

--- index.php
+++ index.php
@@ -7,16 +7,21 @@ session_start();
 		<link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap.min.css">
 		<link rel="stylesheet" href="css/style-index.css">
 	</head>
-	<body>
-	<?php include "php/navbar.php"; ?>
-	<div class="container">
-	<div class="row">
-	<div class="col-md-12">
-			<h2>Sistema de registro de usuarios - ARMY BOX &amp; GYM</h2>
-		
+<body>
+    <?php include "php/navbar.php"; ?>
+    <div class="container">
+    <div class="row">
+    <div class="col-md-12">
+            <h2>Sistema de registro de usuarios - ARMY BOX &amp; GYM</h2>
 
-	</div>
-	</div>
-	</div>
-	</body>
+
+    </div>
+    </div>
+    </div>
+    
+    <!-- Script de bootstrap -->
+    <script src="js/jquery.min.js"></script>
+    <script src="bootstrap/js/bootstrap.js"></script>
+    <script src="bootstrap/js/bootstrap.min.js"></script>
+</body>
 </html>
